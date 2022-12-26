aeb48b325d721be492010fb9b10f2427cf9bbe10
Frida Ruh <35970446+fridaruh@users.noreply.github.com>
2022-12-25 19:02:55 -0600

    Carga de script y archivos de configuración para la aplicación de Streamlit

---
+++ streamlit_cons/main.py
@@ -0,0 +1,102 @@
+# Streamlit-Google Sheet
+## Modules
+import streamlit as st 
+from pandas import DataFrame
+import os
+import openai
+
+from gspread_pandas import Spread,Client
+from google.oauth2 import service_account
+
+from datetime import datetime
+
+# Disable certificate verification (Not necessary always)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+#Connect to openAI
+def openai_connect():
+    openai.api_key = st.secrets["openai_api_key"]
+    
+# Create a Google Authentication connection object
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
+client = Client(scope=scope,creds=credentials)
+spreadsheetname = "Database"
+spread = Spread(spreadsheetname,client = client)
+
+# Check the connection
+st.write(spread.url)
+
+sh = client.open(spreadsheetname)
+worksheet_list = sh.worksheets()
+
+# Functions 
+@st.cache()
+# Get our worksheet names
+def worksheet_names():
+    sheet_names = []   
+    for sheet in worksheet_list:
+        sheet_names.append(sheet.title)  
+    return sheet_names
+
+# Get the sheet as dataframe
+def load_the_spreadsheet(spreadsheetname):
+    worksheet = sh.worksheet(spreadsheetname)
+    df = DataFrame(worksheet.get_all_records())
+    return df
+
+# Update to Sheet
+def update_the_spreadsheet(spreadsheetname,dataframe):
+    col = ['Enlace','Resumen','Time_stamp','What?','So what?']
+    spread.df_to_sheet(dataframe[col],sheet = spreadsheetname,index = False)
+    st.sidebar.info('Updated to GoogleSheet')
+
+
+st.header('Streamlit: Banco de Señales')
+
+st.title('Información de la señal')
+# Introducir el enlace
+st.write('Introduce the link')
+link = st.text_input('Link')
+
+# Introducir el resumen
+st.write('Introduce el resumen')
+summary = st.text_input('Summary')
+
+
+# Mostrar la información
+st.title('Mostrar la información')
+show = st.sidebar.button('Mostrar información OpenAI')
+if show:
+    what = "Get very short abstract in spanish of this text: "+summary
+    why = 'Answer very shortly in spanish why is important this text: '+summary
+    resumen_openai_what = openai_connect().openai.Completion.create(model="text-davinci-003", prompt=what, temperature=0, max_tokens=100)
+    resumen_openai_why = openai_connect().openai.Completion.create(model="text-davinci-003", prompt=why, temperature=0, max_tokens=100)
+
+    st.write('¿Sobre qué trata el texto?')
+    st.write(resumen_openai_what['choices'][0]['text'])
+    st.write('¿Por qué es importante?')
+    st.write(resumen_openai_why['choices'][0]['text'])
+else:
+    st.write('No se ha mostrado la información')
+
+#Agregar entrada de información
+add = st.sidebar.checkbox('Agregar señal')
+if add :  
+    link_nuevo = st.sidebar.text_input('link_nuevo')
+    confirm_input = st.sidebar.button('Confirm')
+    
+    if confirm_input:
+        now = datetime.now()
+        opt = {'Enlace': [link_nuevo],
+                'Resumen' : [summary],
+              'Time_stamp' :  [now],
+              'What?' : [resumen_openai_what['choices'][0]['text']],
+                'So what?' : [resumen_openai_why['choices'][0]['text']]}
+        opt_df = DataFrame(opt)
+        df = load_the_spreadsheet('Hoja 1')
+        new_df = df.append(opt_df,ignore_index=True)
